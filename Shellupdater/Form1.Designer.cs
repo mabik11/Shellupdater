af4b984d29f6bc84edf4ada97cc4408ca2fa0990
Mabik1 <54952224+mabik11@users.noreply.github.com>
2025-07-05 01:55:27 +0300

    Proje dosyası ekle.

---
+++ Shellupdater/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Shellupdater
+    {
+        partial class Form1
+        {
+            private System.ComponentModel.IContainer components = null;
+
+            protected override void Dispose(bool disposing)
+            {
+                if (disposing && (components != null))
+                {
+                    components.Dispose();
+                }
+                base.Dispose(disposing);
+            }
+
+            #region Windows Form Designer generated code
+
+            private void InitializeComponent()
+            {
+                this.txtConsoleOutput = new System.Windows.Forms.RichTextBox();
+                this.btnGuncellemeleriKontrolEt = new System.Windows.Forms.Button();
+                this.btnTumunuGuncelle = new System.Windows.Forms.Button();
+                this.btnWingetYukle = new System.Windows.Forms.Button();
+                this.progressBar1 = new System.Windows.Forms.ProgressBar();
+                this.lblBaslik = new System.Windows.Forms.Label();
+                this.SuspendLayout();
+                // 
+                // txtConsoleOutput
+                // 
+                this.txtConsoleOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                | System.Windows.Forms.AnchorStyles.Left)
+                | System.Windows.Forms.AnchorStyles.Right)));
+                this.txtConsoleOutput.BackColor = System.Drawing.Color.Black;
+                this.txtConsoleOutput.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+                this.txtConsoleOutput.ForeColor = System.Drawing.Color.Lime;
+                this.txtConsoleOutput.Location = new System.Drawing.Point(12, 41);
+                this.txtConsoleOutput.Name = "txtConsoleOutput";
+                this.txtConsoleOutput.ReadOnly = true;
+                this.txtConsoleOutput.Size = new System.Drawing.Size(776, 350);
+                this.txtConsoleOutput.TabIndex = 0;
+                this.txtConsoleOutput.Text = "";
+                // 
+                // btnGuncellemeleriKontrolEt
+                // 
+                this.btnGuncellemeleriKontrolEt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+                this.btnGuncellemeleriKontrolEt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+                this.btnGuncellemeleriKontrolEt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                this.btnGuncellemeleriKontrolEt.ForeColor = System.Drawing.Color.White;
+                this.btnGuncellemeleriKontrolEt.Location = new System.Drawing.Point(12, 397);
+                this.btnGuncellemeleriKontrolEt.Name = "btnGuncellemeleriKontrolEt";
+                this.btnGuncellemeleriKontrolEt.Size = new System.Drawing.Size(180, 41);
+                this.btnGuncellemeleriKontrolEt.TabIndex = 1;
+                this.btnGuncellemeleriKontrolEt.Text = "Güncellemeleri Kontrol Et";
+                this.btnGuncellemeleriKontrolEt.UseVisualStyleBackColor = false;
+                this.btnGuncellemeleriKontrolEt.Click += new System.EventHandler(this.btnCheckUpdates_Click);
+                // 
+                // btnTumunuGuncelle
+                // 
+                this.btnTumunuGuncelle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+                this.btnTumunuGuncelle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+                this.btnTumunuGuncelle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                this.btnTumunuGuncelle.ForeColor = System.Drawing.Color.White;
+                this.btnTumunuGuncelle.Location = new System.Drawing.Point(198, 397);
+                this.btnTumunuGuncelle.Name = "btnTumunuGuncelle";
+                this.btnTumunuGuncelle.Size = new System.Drawing.Size(180, 41);
+                this.btnTumunuGuncelle.TabIndex = 2;
+                this.btnTumunuGuncelle.Text = "Tümünü Güncelle";
+                this.btnTumunuGuncelle.UseVisualStyleBackColor = false;
+                this.btnTumunuGuncelle.Click += new System.EventHandler(this.btnInstallUpdates_Click);
+                // 
+                // btnWingetYukle
+                // 
+                this.btnWingetYukle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+                this.btnWingetYukle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+                this.btnWingetYukle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                this.btnWingetYukle.ForeColor = System.Drawing.Color.White;
+                this.btnWingetYukle.Location = new System.Drawing.Point(608, 397);
+                this.btnWingetYukle.Name = "btnWingetYukle";
+                this.btnWingetYukle.Size = new System.Drawing.Size(180, 41);
+                this.btnWingetYukle.TabIndex = 3;
+                this.btnWingetYukle.Text = "Winget Yükle";
+                this.btnWingetYukle.UseVisualStyleBackColor = false;
+                this.btnWingetYukle.Click += new System.EventHandler(this.btnInstallWinget_Click);
+                // 
+                // progressBar1
+                // 
+                this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                | System.Windows.Forms.AnchorStyles.Right)));
+                this.progressBar1.Location = new System.Drawing.Point(12, 444);
+                this.progressBar1.Name = "progressBar1";
+                this.progressBar1.Size = new System.Drawing.Size(776, 23);
+                this.progressBar1.TabIndex = 4;
+                // 
+                // lblBaslik
+                // 
+                this.lblBaslik.AutoSize = true;
+                this.lblBaslik.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+                this.lblBaslik.ForeColor = System.Drawing.Color.White;
+                this.lblBaslik.Location = new System.Drawing.Point(12, 9);
+                this.lblBaslik.Name = "lblBaslik";
+                this.lblBaslik.Size = new System.Drawing.Size(204, 21);
+                this.lblBaslik.TabIndex = 5;
+                this.lblBaslik.Text = "Winget Güncelleme Aracı";
+                // 
+                // Form1
+                // 
+                this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+                this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+                this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+                this.ClientSize = new System.Drawing.Size(800, 479);
+                this.Controls.Add(this.lblBaslik);
+                this.Controls.Add(this.progressBar1);
+                this.Controls.Add(this.btnWingetYukle);
+                this.Controls.Add(this.btnTumunuGuncelle);
+                this.Controls.Add(this.btnGuncellemeleriKontrolEt);
+                this.Controls.Add(this.txtConsoleOutput);
+                this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+                this.MinimumSize = new System.Drawing.Size(600, 400);
+                this.Name = "Form1";
+                this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+                this.Text = "Winget Güncelleme Aracı";
+                this.Load += new System.EventHandler(this.Form1_Load);
+                this.ResumeLayout(false);
+                this.PerformLayout();
+
+            }
+
+            #endregion
+
+            private System.Windows.Forms.RichTextBox txtConsoleOutput;
+            private System.Windows.Forms.Button btnGuncellemeleriKontrolEt;
+            private System.Windows.Forms.Button btnTumunuGuncelle;
+            private System.Windows.Forms.Button btnWingetYukle;
+            private System.Windows.Forms.ProgressBar progressBar1;
+            private System.Windows.Forms.Label lblBaslik;
+        }
+    }
---
+++ Shellupdater/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Shellupdater
+{
+    public partial class Form1 : Form
+    {
+        private bool isWingetInstalled = false;
+        private string wingetVersion = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckWingetInstallation();
+        }
+
+        private void CheckWingetInstallation()
+        {
+            txtConsoleOutput.AppendText("Winget kurulum durumu kontrol ediliyor...\n");
+
+            try
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/C where winget",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                };
+
+                using (var process = Process.Start(psi))
+                {
+                    string output = process.StandardOutput.ReadToEnd();
+                    string error = process.StandardError.ReadToEnd();
+                    process.WaitForExit();
+
+                    if (string.IsNullOrEmpty(error) && output.Contains("winget.exe"))
+                    {
+                        isWingetInstalled = true;
+                        GetWingetVersion();
+                        return;
+                    }
+                }
+
+                // Winget bulunamadı
+                isWingetInstalled = false;
+                txtConsoleOutput.AppendText("Winget yüklü değil.\n");
+                AskToInstallWinget();
+            }
+            catch (Exception ex)
+            {
+                txtConsoleOutput.AppendText($"Kontrol hatası: {ex.Message}\n");
+            }
+        }
+
+        private void GetWingetVersion()
+        {
+            try
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/C winget --version",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                };
+
+                using (var process = Process.Start(psi))
+                {
+                    wingetVersion = process.StandardOutput.ReadToEnd().Trim();
+                    txtConsoleOutput.AppendText($"Winget yüklü (Sürüm: {wingetVersion})\n\n");
+                }
+            }
+            catch
+            {
+                wingetVersion = "Bilinmeyen";
+            }
+        }
+
+        private void AskToInstallWinget()
+        {
+            var result = MessageBox.Show("Winget yüklü değil. Şimdi yüklemek ister misiniz?",
+                                      "Winget Gerekli",
+                                      MessageBoxButtons.YesNo,
+                                      MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                InstallWinget();
+            }
+            else
+            {
+                txtConsoleOutput.AppendText("Winget yükleme iptal edildi. Uygulama sınırlı işlevsellikle çalışacak.\n");
+            }
+        }
+
+        private void InstallWinget()
+        {
+            try
+            {
+                txtConsoleOutput.AppendText("Winget yükleniyor...\n");
+
+                // GitHub'dan en son Winget release'ini indir
+                string downloadUrl = "https://aka.ms/getwinget";
+                string tempPath = Path.GetTempPath();
+                string installerPath = Path.Combine(tempPath, "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle");
+
+                // Dosyayı indir
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadProgressChanged += (s, e) =>
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            progressBar1.Value = e.ProgressPercentage;
+                        });
+                    };
+
+                    client.DownloadFileCompleted += (s, e) =>
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            if (e.Error != null)
+                            {
+                                txtConsoleOutput.AppendText($"İndirme hatası: {e.Error.Message}\n");
+                                return;
+                            }
+
+                            txtConsoleOutput.AppendText("Yükleyici başarıyla indirildi. Kurulum başlıyor...\n");
+                            InstallWingetPackage(installerPath);
+                        });
+                    };
+
+                    txtConsoleOutput.AppendText("Winget yükleyici indiriliyor...\n");
+                    client.DownloadFileAsync(new Uri(downloadUrl), installerPath);
+                }
+            }
+            catch (Exception ex)
+            {
+                txtConsoleOutput.AppendText($"Yükleme hatası: {ex.Message}\n");
+            }
+        }
+
+        private void InstallWingetPackage(string installerPath)
+        {
+            try
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = "powershell.exe",
+                    Arguments = $"-NoProfile -ExecutionPolicy Bypass -Command \"Add-AppxPackage -Path '{installerPath}'\"",
+                    Verb = "runas", // Yönetici olarak çalıştır
+                    UseShellExecute = true
+                };
+
+                Process.Start(psi);
+                txtConsoleOutput.AppendText("Kurulum başlatıldı. Lütfen bekleyin...\n");
+
+                // Kurulumun bitmesini bekle ve kontrol et
+                System.Timers.Timer timer = new System.Timers.Timer(5000);
+                timer.Elapsed += (s, e) =>
+                {
+                    timer.Stop();
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        CheckWingetInstallation();
+                    });
+                };
+                timer.Start();
+            }
+            catch (Exception ex)
+            {
+                txtConsoleOutput.AppendText($"Kurulum hatası: {ex.Message}\n");
+            }
+        }
+
+        private void ExecuteWingetCommand(string command)
+        {
+            if (!isWingetInstalled)
+            {
+                txtConsoleOutput.AppendText("Hata: Winget yüklü değil.\n");
+                return;
+            }
+
+            txtConsoleOutput.AppendText($"> {command}\n");
+
+            var psi = new ProcessStartInfo
+            {
+                FileName = "winget.exe",
+                Arguments = command,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = false,
+                Verb = "runas"
+            };
+
+            try
+            {
+                using (var process = new Process { StartInfo = psi })
+                {
+                    process.OutputDataReceived += (sender, e) =>
+                    {
+                        if (!string.IsNullOrEmpty(e.Data))
+                        {
+                            this.Invoke((MethodInvoker)delegate
+                            {
+                                txtConsoleOutput.AppendText(e.Data + "\n");
+                                txtConsoleOutput.ScrollToCaret();
+                            });
+                        }
+                    };
+
+                    process.ErrorDataReceived += (sender, e) =>
+                    {
+                        if (!string.IsNullOrEmpty(e.Data))
+                        {
+                            this.Invoke((MethodInvoker)delegate
+                            {
+                                txtConsoleOutput.AppendText("HATA: " + e.Data + "\n");
+                                txtConsoleOutput.ScrollToCaret();
+                            });
+                        }
+                    };
+
+                    process.Start();
+                    process.BeginOutputReadLine();
+                    process.BeginErrorReadLine();
+                    process.WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+                txtConsoleOutput.AppendText($"Komut hatası: {ex.Message}\n");
+            }
+        }
+
+        private void btnCheckUpdates_Click(object sender, EventArgs e)
+        {
+            if (!isWingetInstalled)
+            {
+                CheckWingetInstallation();
+                return;
+            }
+
+            txtConsoleOutput.AppendText("Güncellemeler kontrol ediliyor...\n");
+            ExecuteWingetCommand("upgrade");
+        }
+
+        private void btnInstallUpdates_Click(object sender, EventArgs e)
+        {
+            if (!isWingetInstalled)
+            {
+                CheckWingetInstallation();
+                return;
+            }
+
+            var result = MessageBox.Show("Tüm uygulamalar güncellenecek. Devam etmek istiyor musunuz?",
+                                      "Onay",
+                                      MessageBoxButtons.YesNo,
+                                      MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                txtConsoleOutput.AppendText("Güncellemeler yükleniyor...\n");
+                ExecuteWingetCommand("upgrade --all");
+            }
+        }
+
+        private void btnInstallWinget_Click(object sender, EventArgs e)
+        {
+            InstallWinget();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtConsoleOutput.AppendText("=== Winget Güncelleme Aracı ===\n");
+            txtConsoleOutput.AppendText("1. 'Winget Kontrol Et' - Winget kurulumunu doğrular\n");
+            txtConsoleOutput.AppendText("2. 'Güncellemeleri Kontrol Et' - Kullanılabilir güncellemeleri listeler\n");
+            txtConsoleOutput.AppendText("3. 'Tümünü Güncelle' - Tüm uygulamaları günceller\n\n");
+        }
+    }
+}
